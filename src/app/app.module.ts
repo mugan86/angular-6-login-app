b1eb6582dfaa11bb549177e8b17d5c5b591a102d
Anartz Mugika <mugan86@gmail.com>
2018-09-11 18:29:06 +0200

    Add routes reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// Modules Custom
 import { LoginModule } from './login/login.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
+
+// Routes
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    APP_ROUTES,
     BrowserModule,
     LoginModule,
     PagesModule,
